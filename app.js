f1eeec3ab5a8cd2e9be29550ad8ce9b240949089
Tessema Bey <miamiquniversity@gmail.com>
2023-08-12 01:46:48 -0400

    updated

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-fetch('https://swapi.dev/api/people/?search=r2')
-  .then(response => {
-    return response.json()
-  }).then(json => console.log(json),
-      err => console.log(err))
+function searchCreditTerm() {
+  const searchInput = document.getElementById("searchInput").value;
+  const apiURL = `https://swapi.dev/api/people/?search=${searchInput}`;
 
-      function searchCreditTerm() {
-        const searchInput = document.getElementById('searchInput').value;
-        const apiURL = `https://swapi.dev/api/people/?search=${searchInput}`;
-      
-        fetch(apiURL)
-          .then(response => response.json())
-          .then(data => {
-            const character = data.results[0];
-            const creditTermTitle = document.getElementById('creditTermTitle');
-            const creditTermDefinition = document.getElementById('creditTermDefinition');
-            const starWarsCharacter = document.getElementById('starWarsCharacter');
-      
-            // Update the credit term and definition in the DOM
-            creditTermTitle.textContent = searchInput;
-            creditTermDefinition.textContent = "Definition: Insert definition here (from SWAPI)";
-      
-            // Use the character's name to construct the URL for the character image
-            const characterName = character.name.toLowerCase().replace(/\s/g, '-');
-            starWarsCharacter.src = `https://starwars-visualguide.com/assets/img/characters/${characterName}.jpg`;
-          })
-          .catch(error => console.log(error));
-      }
-      
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const character = data.results[0];
+      const creditTermTitle = document.getElementById("creditTermTitle");
+      const creditTermDefinition = document.getElementById(
+        "creditTermDefinition"
+      );
+      const starWarsCharacter = document.getElementById("starWarsCharacter");
+
+      // Update the credit term and definition in the DOM
+      creditTermTitle.textContent = searchInput;
+      creditTermDefinition.textContent =
+        "Definition: Insert definition here (from SWAPI)";
+
+      // Use the character's name to construct the URL for the character image
+      const characterName = character.name.toLowerCase().replace(/\s/g, "-");
+      starWarsCharacter.src = `https://starwars-visualguide.com/assets/img/characters/${characterName}.jpg`;
+    })
+    .catch((error) => console.log(error));
+}
